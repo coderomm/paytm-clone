de500520b42437c371398b4ba96a41e2cfac633e
coder om <mail.coderom@gmail.com>
2024-06-16 07:48:05 +0530

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:VWthl7v7Bt0dnJbj@cluster0.4kr8zfp.mongodb.net/paytm')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User
+}
