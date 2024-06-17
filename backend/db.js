386c449635c28c71de0121dd728fcb7c5a87ade3
coder om <mail.coderom@gmail.com>
2024-06-17 08:45:15 +0530

    checkpoint: error : not found main.jsx

--- backend/db.js
+++ backend/db.js
@@ -29,10 +29,24 @@ const userSchema = mongoose.Schema({
         trim: true,
         maxLength: 50
     }
-})
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
 
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema)
 
 module.exports = {
-    User
+    User,
+    Account
 }
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const cors = require('cors');
+const PORT = 3000;
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+const mainRouter = require('./routes/index')
+
+app.use('/api/v1', mainRouter);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (e) => {
+  console.error('Error starting server: ', e);
+});
