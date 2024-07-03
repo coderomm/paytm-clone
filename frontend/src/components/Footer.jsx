1ff16cc2f1988a546ce06a15916bec103f745ca4
coder om <mail.coderom@gmail.com>
2024-07-03 23:10:54 +0530

    UI fix in footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
         <footer className="bg-[#163300] flex justify-center items-center">
             <h3 className="text-center text-white py-5 spacing tracking-widest">
                 Made with ❤️ by
-                <a className="underline font-bold" target="_blank" href="https://x.com/1omsharma" rel="noreferrer">Om Sharma</a>
+                <a className="underline font-bold" target="_blank" href="https://x.com/1omsharma" rel="noreferrer"> Om Sharma</a>
             </h3>
         </footer>
     )
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -30,7 +30,7 @@ const Index = () => {
             <footer className="bg-[#16330014] flex justify-center items-center">
                 <h3 className="text-center text-[#163300] py-5 spacing tracking-widest">
                     Made with ❤️ by
-                    <a className="underline font-bold" target="_blank" href="https://x.com/1omsharma" rel="noreferrer">Om Sharma</a>
+                    <a className="underline font-bold" target="_blank" href="https://x.com/1omsharma" rel="noreferrer"> Om Sharma</a>
                 </h3>
             </footer>
         </>
